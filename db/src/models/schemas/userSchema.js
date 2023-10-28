6e8904294b779a9d3a4b1c7a9f313329a19fe959
Lorena De Armas <lorenadearmas01@gmail.com>
2023-10-28 11:22:54 -0300

    user-schema

---
+++ db/src/models/schemas/userSchema.js
@@ -0,0 +1,47 @@
+const { Schema } = require('mongoose');
+
+const userSchema = new Schema({
+    first_name: {
+        type: String,
+        require: true,
+    },
+    last_name: {
+        type: String,
+        require: true,
+    },
+    image: {
+        type: String,
+        require: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        require: true,
+    },
+    password: {
+        type: String,
+        require: true,
+    }
+});
+
+userSchema.statics.list = async function(){
+    return await this.find();
+};
+
+userSchema.statics.getById = async function(id){
+    return await this.findById(id)
+};
+
+userSchema.statics.insert = async function(user){
+    return await this.create(user);
+}
+
+userSchema.statics.update = async function(id, user){
+    return await this.findByIdAndUpdate(id, user);
+};
+
+userSchema.statics.delete = async function(id) {
+    return await this.findByIdAndRemove(id);
+};
+
+module.exports = userSchema;
