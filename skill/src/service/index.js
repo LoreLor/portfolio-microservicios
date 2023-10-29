78d642cada70f1e4764bd8d095434951890ea4fc
Lorena De Armas <lorenadearmas01@gmail.com>
2023-10-29 17:40:28 -0300

    change-localhost-to-db

--- skill/src/service/index.js
+++ skill/src/service/index.js
@@ -2,19 +2,19 @@ const axios = require('axios');
 
 module.exports = {
     list: async () => {
-        const skills = await axios.get('http://localhost:8004/Skill');
+        const skills = await axios.get('http://db:8004/Skill');
         return skills.data;
     },
     getById: async (id) => {
-        const skills = await axios.get(`http://localhost:8004/Skill/${id}`);
+        const skills = await axios.get(`http://db:8004/Skill/${id}`);
         return skills.data;
     },
     insert: async (skill) => {
-        const newSkill = await axios.post('http://localhost:8004/Skill', skill);
+        const newSkill = await axios.post('http://db:8004/Skill', skill);
         return newSkill.data;
     },
     update: async (id, skill) => {
-        const updateSkill = await axios.put(`http://localhost:8004/Skill/${id}`, skill);
+        const updateSkill = await axios.put(`http://db:8004/Skill/${id}`, skill);
         return updateSkill.data;
     },
     delete: async (id) => {
