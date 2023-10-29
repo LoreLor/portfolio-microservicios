e79fce21de62089f51aa69db490fc778979a544b
Lorena De Armas <lorenadearmas01@gmail.com>
2023-10-29 19:38:55 -0300

    rename-db-to-dbs

--- skill/src/service/index.js
+++ skill/src/service/index.js
@@ -2,23 +2,23 @@ const axios = require('axios');
 
 module.exports = {
     list: async () => {
-        const skills = await axios.get('http://db:8004/Skill');
+        const skills = await axios.get('http://dbs:8004/Skill');
         return skills.data;
     },
     getById: async (id) => {
-        const skills = await axios.get(`http://db:8004/Skill/${id}`);
+        const skills = await axios.get(`http://dbs:8004/Skill/${id}`);
         return skills.data;
     },
     insert: async (skill) => {
-        const newSkill = await axios.post('http://db:8004/Skill', skill);
+        const newSkill = await axios.post('http://dbs:8004/Skill', skill);
         return newSkill.data;
     },
     update: async (id, skill) => {
-        const updateSkill = await axios.put(`http://db:8004/Skill/${id}`, skill);
+        const updateSkill = await axios.put(`http://dbs:8004/Skill/${id}`, skill);
         return updateSkill.data;
     },
     delete: async (id) => {
-        const deleteSkill = await axios.delete(`http://localhost:8004/Skill/${id}`);
+        const deleteSkill = await axios.delete(`http://dbs:8004/Skill/${id}`);
         return deleteSkill.data;
     }
 };
