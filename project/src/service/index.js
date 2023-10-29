a625b36914c4b10bb26105cbe849e79d13d3098e
Lorena De Armas <lorenadearmas01@gmail.com>
2023-10-29 18:50:54 -0300

    rename-db-folder-todbs

--- project/src/service/index.js
+++ project/src/service/index.js
@@ -2,23 +2,23 @@ const axios = require('axios');
 
 module.exports = {
     list: async () => {
-        const projects = await axios.get('http://db:8004/Project');
+        const projects = await axios.get('http://dbs:8004/Project');
         return projects.data;
     },
     getById: async (id) => {
-        const projectById = await axios.get(`http://db:8004/Project/${id}`);
+        const projectById = await axios.get(`http://dbs:8004/Project/${id}`);
         return projectById.data;
     },
     insert: async (project) => {
-        const newProject = await axios.post('http://db:8004/Project', project);
+        const newProject = await axios.post('http://dbs:8004/Project', project);
         return newProject.data;
     },
     update: async (id, project) => {
-        const newProject = await axios.put(`http://db:8004/Project/${id}`, project);
+        const newProject = await axios.put(`http://dbs:8004/Project/${id}`, project);
         return newProject.data;
     },
     delete: async (id) => {
-        const deletedProject = await axios.delete(`http://localhost:8004/Project/${id}`);
+        const deletedProject = await axios.delete(`http://dbs:8004/Project/${id}`);
         return deletedProject.data;
     }
 
