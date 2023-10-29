de6d03f353ea21ac8f15eec0cb0e44ee8e31b5f0
Lorena De Armas <lorenadearmas01@gmail.com>
2023-10-29 20:11:37 -0300

    env-PORT

--- dbs/index.js
+++ dbs/index.js
@@ -1,5 +1,6 @@
+const { PORT } = require('./src/config/envs');
 const server = require('./src/server');
 
-server.listen(8004, () => {
-    console.log('DATABASE run on port: 8004')
+server.listen(PORT, () => {
+    console.log(`DATABASE run on port: ${PORT}`)
 });
