2419e4e5562cb25dfc60c67fd57fda47794c7fa8
Lorena De Armas <lorenadearmas01@gmail.com>
2023-10-29 17:39:15 -0300

    change-localhost-service-to-db

--- project/src/service/index.js
+++ project/src/service/index.js
@@ -2,19 +2,19 @@ const axios = require('axios');
 
 module.exports = {
     list: async () => {
-        const projects = await axios.get('http://localhost:8004/Project');
+        const projects = await axios.get('http://db:8004/Project');
         return projects.data;
     },
     getById: async (id) => {
-        const projectById = await axios.get(`http://localhost:8004/Project/${id}`);
+        const projectById = await axios.get(`http://db:8004/Project/${id}`);
         return projectById.data;
     },
     insert: async (project) => {
-        const newProject = await axios.post('http://localhost:8004/Project', project);
+        const newProject = await axios.post('http://db:8004/Project', project);
         return newProject.data;
     },
     update: async (id, project) => {
-        const newProject = await axios.put(`http://localhost:8004/Project/${id}`, project);
+        const newProject = await axios.put(`http://db:8004/Project/${id}`, project);
         return newProject.data;
     },
     delete: async (id) => {
