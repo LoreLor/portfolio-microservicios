a30d27fdf16cb1925e2a3605480b42abe99a2fe1
Lorena De Armas <lorenadearmas01@gmail.com>
2023-10-29 10:46:31 -0300

    sintaxis-error-controller-signin

--- user/src/controllers/signin.js
+++ user/src/controllers/signin.js
@@ -6,16 +6,15 @@ module.exports = async (req, res) => {
     const { email, password } = req.body;
 
     const user = await User.getByEmail(email);
-    console.log('emailUserController', user.data[0].email);
 
     if (user) {
         if (bcrypt.compare(password, user.data[0].password)) {
             return res.status(200).json({
                 message: 'Login Success',
                 data: {
-                    _id: user._id,
-                    first_name: user.first_name,
-                    email: user.email,
+                    _id: user.data[0]._id,
+                    first_name: user.data[0].first_name,
+                    email: user.data[0].email,
                     token: generateToken(user)
                 }
             });
