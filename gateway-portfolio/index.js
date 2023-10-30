2066e644b518d292776461d821d3cd4b9ae98886
Lorena De Armas <lorenadearmas01@gmail.com>
2023-10-30 10:35:47 -0300

    refactoring-envs

--- gateway-portfolio/index.js
+++ gateway-portfolio/index.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const { PORT } = require('./config/envs');
+require('dotenv').config();
 
 const app = express();
+const PORT = process.env.PORT;
 
-app.use(express.json());
 app.use(morgan('dev'));
-app.use(cors());
-
 app.use('/project', createProxyMiddleware({
     // target: 'http://localhost:8001'
     target: 'http://project:8001',
