ed88f700dc9b4934eceb7308ea0cf7ea0ed31e6d
Lorena De Armas <lorenadearmas01@gmail.com>
2023-11-04 10:46:46 -0300

    cors-gateway

--- gateway-portfolio/index.js
+++ gateway-portfolio/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT;
 
+const corsOptions = {
+    origin: 'https://lorena-de-armas.netlify.app',
+    optionsSuccessStatus: 200
+};
+
+app.use(express.json());
 app.use(morgan('dev'));
+app.use(cors(corsOptions));
+
 app.use('/project', createProxyMiddleware({
     // target: 'http://localhost:8001'
     target: 'http://project:8001',
